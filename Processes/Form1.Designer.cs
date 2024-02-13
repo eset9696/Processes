2cd7d3de0adb3cb0dea9d89189effc443fe458ef
sherkunkov@gmail.com <sherkunkov@gmail.com>
2024-02-13 17:03:02 +0800

    Add project files.

---
+++ Processes/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Processes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbRunProcess = new System.Windows.Forms.RichTextBox();
+            this.listViewProcesses = new System.Windows.Forms.ListView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.labelProcessInfo = new System.Windows.Forms.Label();
+            this.myProcess = new System.Diagnostics.Process();
+            this.SuspendLayout();
+            // 
+            // rtbRunProcess
+            // 
+            this.rtbRunProcess.Location = new System.Drawing.Point(13, 12);
+            this.rtbRunProcess.Name = "rtbRunProcess";
+            this.rtbRunProcess.Size = new System.Drawing.Size(333, 40);
+            this.rtbRunProcess.TabIndex = 0;
+            this.rtbRunProcess.Text = "mspaint";
+            // 
+            // listViewProcesses
+            // 
+            this.listViewProcesses.FullRowSelect = true;
+            this.listViewProcesses.HideSelection = false;
+            this.listViewProcesses.Location = new System.Drawing.Point(352, 12);
+            this.listViewProcesses.MultiSelect = false;
+            this.listViewProcesses.Name = "listViewProcesses";
+            this.listViewProcesses.Size = new System.Drawing.Size(478, 426);
+            this.listViewProcesses.TabIndex = 1;
+            this.listViewProcesses.UseCompatibleStateImageBehavior = false;
+            this.listViewProcesses.View = System.Windows.Forms.View.Details;
+            this.listViewProcesses.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listViewProcesses_MouseDoubleClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 58);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(271, 58);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // labelProcessInfo
+            // 
+            this.labelProcessInfo.AutoSize = true;
+            this.labelProcessInfo.Location = new System.Drawing.Point(13, 88);
+            this.labelProcessInfo.Name = "labelProcessInfo";
+            this.labelProcessInfo.Size = new System.Drawing.Size(44, 16);
+            this.labelProcessInfo.TabIndex = 4;
+            this.labelProcessInfo.Text = "label1";
+            // 
+            // myProcess
+            // 
+            this.myProcess.StartInfo.Domain = "";
+            this.myProcess.StartInfo.LoadUserProfile = false;
+            this.myProcess.StartInfo.Password = null;
+            this.myProcess.StartInfo.StandardErrorEncoding = null;
+            this.myProcess.StartInfo.StandardOutputEncoding = null;
+            this.myProcess.StartInfo.UserName = "";
+            this.myProcess.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 450);
+            this.Controls.Add(this.labelProcessInfo);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.listViewProcesses);
+            this.Controls.Add(this.rtbRunProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbRunProcess;
+        private System.Windows.Forms.ListView listViewProcesses;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label labelProcessInfo;
+        private System.Diagnostics.Process myProcess;
+    }
+}
+
---
+++ Processes/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Processes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listViewProcesses.Columns.Add("PID");
+            listViewProcesses.Columns.Add("Name");
+        }
+
+        void InitProcess()
+        {
+            myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(rtbRunProcess.Text);
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            InitProcess();
+            myProcess.Start();
+            listViewProcesses.Items.Add(myProcess.Id.ToString());
+            listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems.Add(myProcess.ProcessName.ToString());
+            ProcessInfo();
+            myProcess = new System.Diagnostics.Process();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myProcess = System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems[0].Text));
+                myProcess.CloseMainWindow();
+                myProcess.Close();
+                listViewProcesses.Items.RemoveAt(listViewProcesses.Items.Count - 1);
+
+            }
+            catch (Exception currentItemIsNullException)
+            {
+                listViewProcesses.Items.RemoveAt(listViewProcesses.Items.Count - 1);
+            }finally { ProcessInfo(); }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            while (listViewProcesses.Items.Count > 0)
+            {
+                btnStop_Click (sender, e);
+            }
+        }
+
+        void ProcessInfo()
+        {
+            if (listViewProcesses.Items.Count > 0)
+            {
+                System.Diagnostics.Process process =
+                    //listViewProcesses.SelectedItems[0] != null ?
+                    //System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.SelectedItems[0].SubItems[0].Text)):
+                    System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems[0].Text));
+                labelProcessInfo.Text = $"Processes count: {listViewProcesses.Items.Count}\n";
+                labelProcessInfo.Text += $"PID: {process.Id}\n";
+                labelProcessInfo.Text += $"Process name: {process.ProcessName}\n";
+                labelProcessInfo.Text += $"Start time: {process.StartTime}\n";
+                labelProcessInfo.Text += $"Processor time: {process.TotalProcessorTime}\n";
+                labelProcessInfo.Text += $"Memory: {process.PagedMemorySize}\n"; 
+            }
+            else
+            {
+                labelProcessInfo.Text = "Список процессов пуст";
+            }
+        }
+
+        void ProcessInfo1()
+        {
+            if (listViewProcesses.Items.Count > 0)
+            {
+                System.Diagnostics.Process process = System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.SelectedItems[0].SubItems[0].Text));
+                    //System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems[0].Text));
+                labelProcessInfo.Text = $"Processes count: {listViewProcesses.Items.Count}\n";
+                labelProcessInfo.Text += $"PID: {process.Id}\n";
+                labelProcessInfo.Text += $"Process name: {process.ProcessName}\n";
+                labelProcessInfo.Text += $"Start time: {process.StartTime}\n";
+                labelProcessInfo.Text += $"Processor time: {process.TotalProcessorTime}\n";
+                labelProcessInfo.Text += $"Memory: {process.PagedMemorySize}\n";
+            }
+            else
+            {
+                labelProcessInfo.Text = "Список процессов пуст";
+            }
+        }
+
+        private void listViewProcesses_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ProcessInfo1();
+
+        }
+    }
+}
