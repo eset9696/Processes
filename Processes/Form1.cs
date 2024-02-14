43b54f3e38db01df17bc7d5bdd5f2a5a6f3fbe44
Mikhail Sherkunkov <sherkunkov@gmail.com>
2024-02-14 17:24:02 +0800

    info with process

--- Processes/Form1.cs
+++ Processes/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,6 @@ namespace Processes
         void InitProcess()
         {
             myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(rtbRunProcess.Text);
-
         }
 
         private void btnStart_Click(object sender, EventArgs e)
@@ -31,7 +31,7 @@ namespace Processes
             myProcess.Start();
             listViewProcesses.Items.Add(myProcess.Id.ToString());
             listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems.Add(myProcess.ProcessName.ToString());
-            ProcessInfo();
+            ProcessInfo(myProcess);
             myProcess = new System.Diagnostics.Process();
         }
 
@@ -48,7 +48,12 @@ namespace Processes
             catch (Exception currentItemIsNullException)
             {
                 listViewProcesses.Items.RemoveAt(listViewProcesses.Items.Count - 1);
-            }finally { ProcessInfo(); }
+            }
+            finally 
+            {
+				ProcessInfo(listViewProcesses.Items.Count > 0 ? System.Diagnostics.Process.GetProcessById(
+								Convert.ToInt32(listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems[0].Text)) : null);
+			}
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -59,14 +64,10 @@ namespace Processes
             }
         }
 
-        void ProcessInfo()
+        void ProcessInfo(System.Diagnostics.Process process)
         {
             if (listViewProcesses.Items.Count > 0)
             {
-                System.Diagnostics.Process process =
-                    //listViewProcesses.SelectedItems[0] != null ?
-                    //System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.SelectedItems[0].SubItems[0].Text)):
-                    System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems[0].Text));
                 labelProcessInfo.Text = $"Processes count: {listViewProcesses.Items.Count}\n";
                 labelProcessInfo.Text += $"PID: {process.Id}\n";
                 labelProcessInfo.Text += $"Process name: {process.ProcessName}\n";
@@ -80,28 +81,9 @@ namespace Processes
             }
         }
 
-        void ProcessInfo1()
-        {
-            if (listViewProcesses.Items.Count > 0)
-            {
-                System.Diagnostics.Process process = System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.SelectedItems[0].SubItems[0].Text));
-                    //System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.Items[listViewProcesses.Items.Count - 1].SubItems[0].Text));
-                labelProcessInfo.Text = $"Processes count: {listViewProcesses.Items.Count}\n";
-                labelProcessInfo.Text += $"PID: {process.Id}\n";
-                labelProcessInfo.Text += $"Process name: {process.ProcessName}\n";
-                labelProcessInfo.Text += $"Start time: {process.StartTime}\n";
-                labelProcessInfo.Text += $"Processor time: {process.TotalProcessorTime}\n";
-                labelProcessInfo.Text += $"Memory: {process.PagedMemorySize}\n";
-            }
-            else
-            {
-                labelProcessInfo.Text = "Список процессов пуст";
-            }
-        }
-
         private void listViewProcesses_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            ProcessInfo1();
+		{
+            ProcessInfo(System.Diagnostics.Process.GetProcessById(Convert.ToInt32(listViewProcesses.SelectedItems[0].SubItems[0].Text)));
 
         }
     }
